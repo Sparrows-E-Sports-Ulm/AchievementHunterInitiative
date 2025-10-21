46d4be7fb787622b3c637a40fe908dfbfcefa6e9
JustinMuecke <justin.muecke@gmail.com>
2025-10-21 19:34:01 +0200

    feat(project): Cleanup
    Removed old code to start new

--- command.py
+++
@@ -1,9 +0,0 @@
-from command_type import command_type
-
-class command:
-    type = command_type
-    entity : str
-
-    def __init__(self, command, entity):
-        self.type = command
-        self.entity = entity
--- command_type.py
+++
@@ -1,4 +0,0 @@
-import enum
-class command_type(enum.Enum):
-    REGISTER = "register"
-    UPDATE = "update"
